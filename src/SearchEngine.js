022a5671f9b2f0b3510b8e665f0a9781866eab2f
Maribau11 <maribau11@gmail.com>
2023-09-19 23:22:31 +0200

    Coded some of the functionality of the search component

--- src/SearchEngine.js
+++ src/SearchEngine.js
@@ -1,11 +1,24 @@
-import React from "react"
+import React, { useState }from "react"
+import CurrentWeather from "./CurrentWeather";
 
 export default function SearchEngine(){
+    let [city, setCity]= useState("");
+    let [showInfo, setShowInfo]=useState(false);
+
+    function handleSubmit(event){
+        event.preventDefault();
+        setShowInfo(true);
+    }
+
+    function changeCity(event){
+        event.preventDefault();
+        setCity(event.target.value);
+    }
     return(
-        <form>
-            <input type="text" placeholder="Search a city"/>
+        <form onSubmit={handleSubmit}>
+            <input type="text" placeholder="Search a city" onChange={changeCity}/>
             <input type="submit" value="Search"/>
-            <input type="submit" value="Current"/>
+            {showInfo && <CurrentWeather/>}
         </form>
     )
 }
