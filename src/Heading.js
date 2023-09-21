55668b34a6bbe67bc4d9379c46cb7142d5e1317f
Maribau11 <maribau11@gmail.com>
2023-09-21 10:01:43 +0200

    updated the heading component

--- src/Heading.js
+++ src/Heading.js
@@ -1,11 +1,13 @@
-import React from "react"
+import React,{useState} from "react"
 import "bootstrap/dist/css/bootstrap.min.css";
 
-export default function Heading(){
+export default function Heading(props){
+  let [city,setCity] = useState("Cape Town");
+  
     return (
       <div className="heading">
         <div className="row">
-          <div className="col">City Name</div>
+          <div className="col">{props.cityName}</div>
           <div className="col">Current Weather Icon</div>
           <div className="col">Current Date</div>
         </div>
